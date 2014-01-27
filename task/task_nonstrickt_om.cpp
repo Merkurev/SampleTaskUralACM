6e73d4d5e5ef17599632a6f45712c9a223c95e63
Merkurev <olemerkure@mail.ru>
2014-01-28 01:17:55 +0600

    забыты #ifndef ONLINE_JUDGE

--- task/task_nonstrickt_om.cpp
+++ task/task_nonstrickt_om.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int main()
 {
+#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	
+#endif	
 	int a, b;
 	scanf("%d%d", &a, &b);
 	int x = (a + b - 4) + rand() % 8;
